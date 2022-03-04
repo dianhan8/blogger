3ce65b3471cb9e494b821c6f52eaaefbab41bb15
dianhan8 <55011398+dianhan8@users.noreply.github.com>
2022-03-04 20:12:18 +0700

    Update pencarian.js

--- pencarian.js
+++ pencarian.js
@@ -10,6 +10,14 @@ const Pencarian = () => {
     }
   );
 
+  const classContentModal = classNames(
+    "relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full",
+    {
+      "opacity-0 ease-out duration-200 translate-y-4 sm:translate-y-0 sm:scale-95": !visible,
+      "opacity-100 ease-in duration-300 opacity-100 translate-y-0 sm:scale-100": visible,
+    }
+  )
+
   return (
     <div>
       <button className="w-full px-4 py-2 flex items-center shadow-md rounded-md focus:ring-2 focus:ring-blue-500">
@@ -23,14 +31,14 @@ const Pencarian = () => {
         aria-modal="true"
       >
         <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-          <div className={classOverplay} />
+          <div className={classOverplay} onClick={() => setVisible(false)} />
           <span
             className="hidden sm:inline-block sm:align-middle sm:h-screen"
             aria-hidden="true"
           >
             &#8203;
           </span>
-          <div className="relative inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
+          <div className={classContentModal}>
             <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
               <div className="sm:flex sm:items-start">
                 <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
