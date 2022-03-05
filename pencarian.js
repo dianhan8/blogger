61111cbe460368b981cee0f9623e67abe2bcaeea
dianhan8 <55011398+dianhan8@users.noreply.github.com>
2022-03-05 19:43:51 +0700

    Update pencarian.js

--- pencarian.js
+++ pencarian.js
@@ -67,14 +67,8 @@ const Pencarian = () => {
         role="dialog"
         aria-modal="true"
       >
-        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+        <div class="flex min-h-screen items-center justify-center">
           <div className={classOverplay} onClick={() => setVisible(false)} />
-          <span
-            className="hidden sm:inline-block sm:align-middle sm:h-screen"
-            aria-hidden="true"
-          >
-            &#8203;
-          </span>
           <div className={classContentModal}>
             <div className="flex w-full items-center px-6 py-4">
               {isLoading ? (
